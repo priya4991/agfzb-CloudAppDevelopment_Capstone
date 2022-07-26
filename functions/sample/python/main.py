8443dee41b6579bd94c8e07c07c13edbe4ebd6ea
priya4991 <pdutta94@gmail.com>
2022-07-26 15:42:20 +0000

    update to functions

--- functions/sample/python/main.py
+++ functions/sample/python/main.py
@@ -18,72 +18,68 @@ def main(dict):
     databaseName = "reviews"
     CLOUDANT_APIKEY = ''
     CLOUDANT_URL = ''
-    
+
     try:
         authenticator = IAMAuthenticator(CLOUDANT_APIKEY)
 
         client = CloudantV1(authenticator=authenticator)
 
         client.set_service_url(CLOUDANT_URL)
-        
+
         if 'dealerId' in dict.keys():
             sel = {
                 "dealership": int(dict['dealerId'])
             }
-    
-            result = client.post_find(db=databaseName,selector=sel).get_result()['docs']
+
+            result = client.post_find(
+                db=databaseName, selector=sel).get_result()['docs']
             if len(result) > 0:
                 refined_result = list(map(refine, result))
-    
-    
+
                 return {"reviews": refined_result}
-            
+
             else:
                 return {"error": {
                     "statusCode": 404,
                     "message": "dealerId does not exist"
                 }}
-    
+
         elif 'id' in dict.keys():
             reviews_doc = Document(
-            ID=dict['id'],
-            name=dict['name'],
-            dealership=dict['dealership'],
-            review=dict['review'],
-            purchase=True if dict['purchase'] == "true" else False,
-            another=dict['another'],
-            purchase_date=dict['purchase_date'],
-            car_make=dict['car_make'],
-            car_model=dict['car_model'],
-            car_year=dict['car_year']
+                ID=dict['id'],
+                name=dict['name'],
+                dealership=dict['dealership'],
+                review=dict['review'],
+                purchase=True if dict['purchase'] == "true" else False,
+                another=dict['another'],
+                purchase_date=dict['purchase_date'],
+                car_make=dict['car_make'],
+                car_model=dict['car_model'],
+                car_year=dict['car_year']
             )
 
-            response = client.post_document(db=databaseName, document=reviews_doc).get_result()
+            response = client.post_document(
+                db=databaseName, document=reviews_doc).get_result()
             return {"response": response}
-        
-        else: 
+
+        else:
             return {"error": {
-            "statusCode": 500,
-            "message": "Something went wrong on the server"
-        }}
+                "statusCode": 500,
+                "message": "Something went wrong on the server"
+            }}
 
-
-        
     except ApiException as ce:
         return {"error": {
             "statusCode": 500,
             "message": "Something went wrong on the server"
         }}
-    
+
     except (requests.exceptions.RequestException, ConnectionResetError) as err:
         print("connection error")
         return {"error": err}
 
-    
-    
-def refine(res): 
+
+def refine(res):
     res.pop('_id')
     res.pop('_rev')
     return res
-
-
