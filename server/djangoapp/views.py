cb98846fa1df7cccda99fcc6ffa70ee1f1179e81
priya <pdutta94@gmail.com>
2022-07-31 15:08:25 +0000

    added table

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -94,14 +94,20 @@ def registration_request(request):
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
+    context = {}
     if request.method == "GET":
         url = "https://6c22589a.us-south.apigw.appdomain.cloud/api/dealership"
         # Get dealers from the URL
         dealerships = get_dealers_from_cf(url)
+        context['dealerships'] = dealerships
         # Concat all dealer's short name
         dealer_names = ' '.join([dealer.short_name for dealer in dealerships])
         # Return a list of dealer short name
-        return HttpResponse(dealer_names)
+        #return HttpResponse(dealer_names)
+        return render(request, 'djangoapp/index.html', context) 
+    
+    else: 
+        return HttpResponse('<h1>hi</h1>')
 
 
 def get_dealers_by_state(request, state):
